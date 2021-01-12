3c9229f5d455220b58e9a24f7d55cf74db4062d5
VitorLimaRios <vitorlimarios999@gmail.com>
2021-01-12 00:48:28 -0300

    adicionando exercicios

---
+++ bloco_11/dia_1/pedrinho/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class myName extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Vitor Lima Rios</h1>
+        <p>Amante de anime e fanático por jogos.</p>
+      </div>
+    )
+  }
+}
+
+export default myName;
