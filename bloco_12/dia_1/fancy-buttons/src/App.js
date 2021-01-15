b255b927fdbcb2209fca37f2dbd58d6f4d1d3c67
VitorLimaRios <vitorlimarios999@gmail.com>
2021-01-15 15:32:51 -0300

    adicionando alterações

--- bloco_12/dia_1/fancy-buttons/src/App.js
+++ bloco_12/dia_1/fancy-buttons/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.handleClick = this.handleClick.bind(this)
+    this.botãoSegundo = this.botãoSegundo.bind(this)
+    this.botãoTche = this.botãoTche.bind(this)
+
+    
+  }
+
+  handleClick() {
+    console.log('Clicou no botão!')
+  }
+  
+  botãoSegundo() {
+    console.log('Esse é o segundo!')
+  }
+  
+  botãoTche() {
+    console.log('Bagual!')
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Meu botão</button>
+        <button onClick={this.botãoSegundo}>Segundo botão</button>
+        <button onClick={this.botãoTche}>Tche botão</button>
+      </div>
+    )
+  }
 }
 
 export default App;
