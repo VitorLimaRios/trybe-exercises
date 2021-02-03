580027f60ced4ea6ed655989730d53d9714fb237
VitorLimaRios <vitorlimarios999@gmail.com>
2021-02-03 17:53:12 -0300

    terminando exercicio

--- bloco_12/dia_1/fancy-buttons/src/App.js
+++ bloco_12/dia_1/fancy-buttons/src/App.js
@@ -2,34 +2,45 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.handleClick = this.handleClick.bind(this)
     this.botãoSegundo = this.botãoSegundo.bind(this)
     this.botãoTche = this.botãoTche.bind(this)
 
-    
+    this.state = {
+      meuBotao: 0,
+      segundo: 0,
+      tche: 0,
+    }
   }
 
   handleClick() {
-    console.log('Clicou no botão!')
+    this.setState((estadoAnterior, _props) => ({
+      meuBotao: estadoAnterior.meuBotao + 1
+    }))
   }
   
   botãoSegundo() {
-    console.log('Esse é o segundo!')
+    this.setState((estadoAnterior, _props) => ({
+      segundo: estadoAnterior.segundo + 1
+    }))
   }
   
   botãoTche() {
-    console.log('Bagual!')
+    this.setState((estadoAnterior, _props) => ({
+      tche: estadoAnterior.tche + 1
+    }))
   }
 
   render() {
+    const { meuBotao, segundo, tche } = this.state;
     return (
       <div>
-        <button onClick={this.handleClick}>Meu botão</button>
-        <button onClick={this.botãoSegundo}>Segundo botão</button>
-        <button onClick={this.botãoTche}>Tche botão</button>
+        <button onClick={this.handleClick}>{meuBotao}</button>
+        <button onClick={this.botãoSegundo}>{segundo}</button>
+        <button onClick={this.botãoTche}>{tche}</button>
       </div>
     )
   }
